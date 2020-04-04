9bbef7aacfc8dd795f13dc2990bf6a4437783f46
Hakan Akın <cehakanakin@gmail.com>
2020-04-04 14:14:04 +0300

    Added enemy and animations

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var a = anim.runtimeAnimatorController.animationClips;
+            anim.Play("TurnAndShoot");
+        }
+    }
+}
